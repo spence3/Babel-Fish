99a01ede9dc966e065e6edd2144c7d11b0f6fcc2
Spencer Miller <spencerrmiller3@gmail.com>
2024-11-04 14:33:43 -0700

    translation working

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express()
 const http = require('http')
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+require('dotenv').config();
+
+const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+const model = genAI.getGenerativeModel({model:"gemini-1.5-flash"})
 
 const server = http.createServer(app)
 
@@ -9,16 +14,27 @@ const io = require('socket.io')(server, {
     cors: {origin:"*"}
 })
 
-io.on('connection', (socket) => {
-    console.log('user connected')
-
-    socket.on('message', (message) => {
-        console.log('message', `${socket.id.substr(0,2)} said ${message}`)
-        io.emit('message', `${socket.id.substr(0,2)} said ${message}`)
+function run(){
+    io.on('connection', (socket) => {
+        console.log('user connected')
+        socket.on('message', async (message) => {
+            try{
+                console.log(`message before translation ${message}`)
+                const prompt = `only respond with ${message} translated into spanish`
+                const result = await model.generateContent(prompt)
+                console.log('message', `${socket.id.substr(0,2)} said ${message}`)
+                await io.emit('message', `${socket.id.substr(0,2)} said: ${message} \n Translation: ${result.response.text()}.`)
+            }
+            catch(error){
+                console.error("error with translate!", error)
+            }
+            
+        })
     })
-})
-
+}
+    
 
 server.listen(8000, () => {
     console.log('server running on http://localhost:8000')
+    run()
 })
