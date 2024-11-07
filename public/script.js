c2fcf286d94d38fea5d4cffbc364026c2216ebee
Spencer Miller <spencerrmiller3@gmail.com>
2024-11-07 13:49:02 -0700

    cleanup code

--- public/script.js
+++ public/script.js
@@ -1,8 +1,9 @@
-const socket = io('ws://localhost:8000'); // Connect to server
+const socket = io('ws://localhost:8000') // Connect to server
 
-//text to speech
-const synth = window.speechSynthesis;
-const voices = synth.getVoices()
+//speech to text
+const synth = window.speechSynthesis
+const voices = synth.getVoices()//populate voices
+
 //text to speech variables
 var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition
 var SpeechGrammarList = SpeechGrammarList || window.webkitSpeechGrammarList
@@ -31,7 +32,6 @@ voiceButton.onclick = function() {
 recognition.onresult = function(event) {
   text = event.results[0][0].transcript
   diagnostic.value += text + '.'
-  console.log(text)
   console.log('Confidence: ' + event.results[0][0].confidence)
 }
 
@@ -46,37 +46,39 @@ recognition.onerror = function(event) {
   diagnostic.textContent = 'Error occurred in recognition: ' + event.error
 }
 
+
+//text to speech
 function speakMessage(spanishText){
   if (synth.speaking) {
-    console.error("speechSynthesis.speaking");
-    return;
+    console.error("speechSynthesis.speaking")
+    return
   }
 
   if (true) {
-    // const utterThis = new SpeechSynthesisUtterance(inputTxt.value);
-    const utterThis = new SpeechSynthesisUtterance(spanishText);//----------
+    //going to change this to whatever language it's translated into
+    const utterThis = new SpeechSynthesisUtterance(spanishText)
     
 
     //done talking
     utterThis.onend = function (event) {
-      console.log("SpeechSynthesisUtterance.onend");
-    };
+      console.log("SpeechSynthesisUtterance.onend")
+    }
 
     //error with speech synthesis
     utterThis.onerror = function (event) {
-      console.error("SpeechSynthesisUtterance.onerror");
-    };
-    // console.log(voiceSelect)
+      console.error("SpeechSynthesisUtterance.onerror")
+    }
 
+    //voice = spanish or english
     const englishVoice = 'Aaron'
     const spanishVoice = 'Flo (Spanish (Mexico))'
-    const findMyVoice = voices.find(voice => voice.name === englishVoice);
+    const findMyVoice = voices.find(voice => voice.name === englishVoice)
 
     utterThis.voice = findMyVoice
-    utterThis.pitch = 1;//-------------
-    utterThis.rate = 1;//-------------
+    utterThis.pitch = 1
+    utterThis.rate = 1
 
-    synth.speak(utterThis);
+    synth.speak(utterThis)
   }
 }
 
@@ -89,7 +91,7 @@ socket.on('message', message => {
 sendButton.onclick = function() {
   // var translateDisplay = displayMessage.value += text + '\n'
   const send = text
-  socket.emit('message', send);
-  diagnostic.value = ''; // Clear the input after sending
+  socket.emit('message', send)
+  diagnostic.value = '' // Clear the input after sending
 
 }
